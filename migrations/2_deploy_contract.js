f2e452605b09c916df82c919cf16f890f9b3b8a4
Pao - Wasut Panyawiphat <wasut_pao@hotmail.com>
2022-02-06 16:41:52 +0700

    add deploy script

---
+++ migrations/2_deploy_contract.js
@@ -0,0 +1,9 @@
+const ToonSurvival = artifacts.require("./ToonSurvival.sol");
+
+module.exports = (deployer, network, accounts) => {
+  // IPFS URI should have "/" at the end.
+  const baseUri = 'ipfs://xxxxx/';
+  const hiddenUri = "ipfs:/xxxxx/";
+
+  deployer.deploy(ToonSurvival, baseUri, hiddenUri);
+};
