7b46fb176831227e93ddf1bf8ceba2193398540d
Pao - Wasut Panyawiphat <wasut_pao@hotmail.com>
2022-02-12 16:45:03 +0700

    fixed multiple mint issue, refactor code and add unit test

--- contracts/ToonSurvival.sol
+++ contracts/ToonSurvival.sol
@@ -8,45 +8,36 @@ contract ToonSurvival is ERC721A, Ownable {
   using Strings for uint256;
 
   string baseURI;
-  string public hiddenURI;
+  string public hiddenBaseURI;
   uint256 public cost = 0.1 ether;
   uint256 public maxSupply = 100;
-  uint256 public maxMintAmount = 5;
+  uint256 public maxMintAmountPerTx = 5;
   bool public paused = false;
   bool public revealed = false;
 
   constructor(
     string memory _initBaseURI,
-    string memory _initHiddenURI
-  ) ERC721A("Toon Survival", "TS") {
+    string memory _initHiddenBaseURI
+  ) ERC721A("Toon Survival", "TSVR") {
     baseURI = _initBaseURI;
-    hiddenURI = _initHiddenURI;
+    hiddenBaseURI = _initHiddenBaseURI;
   }
 
-  // internal
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
+  modifier mintCompliance(uint256 _mintAmount) {
+    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");
+    require(totalSupply() + _mintAmount <= maxSupply, "Max supply exceeded!");
+    _;
   }
 
-  function _hiddenURI() internal view virtual returns (string memory) {
-    return hiddenURI;
+  function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) {
+    require(!paused, "The contract is paused!");
+    require(msg.value >= cost * _mintAmount, "Insufficient funds!");
+
+    _safeMint(msg.sender, _mintAmount);
   }
 
-  // public
-  function mint(uint256 _mintAmount) public payable {
-    uint256 supply = totalSupply();
-    require(!paused);
-    require(_mintAmount > 0);
-    require(_mintAmount <= maxMintAmount);
-    require(supply + _mintAmount <= maxSupply);
-
-    if (msg.sender != owner()) {
-      require(msg.value >= cost * _mintAmount);
-    }
-
-    for (uint256 i = 1; i <= _mintAmount; i++) {
-      _safeMint(msg.sender, supply + i);
-    }
+  function mintForAddress(uint256 _mintAmount, address _receiver) public mintCompliance(_mintAmount) onlyOwner {
+    _safeMint(_receiver, _mintAmount);
   }
 
   function walletOfOwner(address _owner)
@@ -74,7 +65,7 @@ contract ToonSurvival is ERC721A, Ownable {
       "ERC721Metadata: URI query for nonexistent token"
     );
 
-    string memory currentBaseURI = revealed ? _baseURI() : _hiddenURI();
+    string memory currentBaseURI = revealed ? _baseURI() : hiddenBaseURI;
 
     return bytes(currentBaseURI).length > 0
         ? string(abi.encodePacked(currentBaseURI, tokenId.toString()))
@@ -85,28 +76,32 @@ contract ToonSurvival is ERC721A, Ownable {
     baseURI = _newBaseURI;
   }
 
-  function setHiddenURI(string memory _newHiddenURI) public onlyOwner {
-    hiddenURI = _newHiddenURI;
+  function setHiddenBaseURI(string memory _newHiddenBaseURI) public onlyOwner {
+    hiddenBaseURI = _newHiddenBaseURI;
   }
 
   function setCost(uint256 _cost) public onlyOwner() {
     cost = _cost;
   }
 
-  function setMaxMintAmount(uint256 _maxMintAmount) public onlyOwner() {
-    maxMintAmount = _maxMintAmount;
+  function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner() {
+    maxMintAmountPerTx = _maxMintAmountPerTx;
   }
 
-  function pause(bool _state) public onlyOwner {
-    paused = _state;
+  function setPaused(bool _paused) public onlyOwner {
+    paused = _paused;
   }
 
-  function setReveal(bool _revealed) public onlyOwner {
-      revealed = _revealed;
+  function setRevealed(bool _revealed) public onlyOwner {
+    revealed = _revealed;
   }
 
   function withdraw() public payable onlyOwner {
     (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
     require(success);
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
   }
 }
---
+++ test/ToonSurvivalTest.js
@@ -0,0 +1,95 @@
+const {expect} = require('chai');
+const {expectRevert} = require('@openzeppelin/test-helpers');
+const web3 = require('web3');
+
+const ToonSurvival = artifacts.require("ToonSurvival");
+
+contract("ToonSurvival", accounts => {
+    let toonSurvival = null;
+    const baseUri = "ipfs://baseUri/";
+    const hiddenBaseUri = "ipfs://hiddenBaseUri/";
+
+    beforeEach(async () => {
+        toonSurvival = await ToonSurvival.new(baseUri, hiddenBaseUri);
+    });
+
+    it('should has 0 totalSupply with no minted token', async () => {
+        const supply = await toonSurvival.totalSupply();
+
+        expect(supply.toNumber()).to.equal(0);
+    });
+
+    it('should paused by default', async () => {
+        const paused = await toonSurvival.paused();
+
+        expect(paused).to.equal(false);
+    });
+
+    it('should return empty walletOfOwner with no minted token', async () => {
+        const ids = await toonSurvival.walletOfOwner(accounts[0]);
+
+        expect(ids.length).to.equal(0);
+    });
+
+    it('should mint failed when mint amount is 0', async () => {
+        await expectRevert(toonSurvival.mint(0, {from: accounts[0]}), "Invalid mint amount!");
+    });
+
+    it('should mint failed when mintAmount is greater than maxMintAmountPerTx', async () => {
+        await toonSurvival.setPaused(true);
+        await expectRevert(toonSurvival.mint(6, {
+            from: accounts[0],
+            value:  web3.utils.toWei('0.1', 'ether')
+        }), "Invalid mint amount!");
+    });
+
+    it('should mint failed when insufficient fund', async () => {
+        await expectRevert(toonSurvival.mint(1, {from: accounts[0]}), "Insufficient funds!");
+    });
+
+    it('should mint failed when contract is paused', async () => {
+        await toonSurvival.setPaused(true);
+        await expectRevert(toonSurvival.mint(1, {
+            from: accounts[0],
+            value:  web3.utils.toWei('0.1', 'ether')
+        }), "The contract is paused!");
+    });
+
+    it('should be able mintForAddress even the contract is paused', async () => {
+        await toonSurvival.setPaused(true);
+        await toonSurvival.mintForAddress(1, accounts[1], {from: accounts[0]});
+
+        const supply = await toonSurvival.totalSupply();
+        const walletOfOwner = await toonSurvival.walletOfOwner(accounts[1]);
+
+        expect(supply.toNumber()).to.equal(1);
+        expect(walletOfOwner.length).to.equal(1);
+    });
+
+    it('should have token when mint success', async () => {
+        await toonSurvival.mint(2, {from: accounts[0], value: web3.utils.toWei('0.2', 'ether')});
+
+        const supply = await toonSurvival.totalSupply();
+        const walletOfOwner = await toonSurvival.walletOfOwner(accounts[0]);
+
+        expect(supply.toNumber()).to.equal(2);
+        expect(walletOfOwner.length).to.equal(2);
+    });
+
+    it('should get hiddenBaseURI with token id when get tokenURI and revealed is false', async () => {
+        await toonSurvival.mint(2, {from: accounts[0], value: web3.utils.toWei('0.2', 'ether')});
+
+        const tokenUri = await toonSurvival.tokenURI(1);
+
+        expect(tokenUri).to.equal(hiddenBaseUri + "1");
+    });
+
+    it('should get baseUri with token id when get tokenURI and revealed is tru', async () => {
+        await toonSurvival.setRevealed(true);
+        await toonSurvival.mint(2, {from: accounts[0], value: web3.utils.toWei('0.2', 'ether')});
+
+        const tokenUri = await toonSurvival.tokenURI(1);
+
+        expect(tokenUri).to.equal(baseUri + "1");
+    });
+});
