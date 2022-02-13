6dcbe797b2a7510c92045fbca24d91b252b54ebb
Pao - Wasut Panyawiphat <wasut_pao@hotmail.com>
2022-02-13 15:04:33 +0700

    refactor to single quote, update readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ## Toon Survival NFT
-100 Unique NFT
+100 Toon Survival Unique NFT
 Just for testing ERC721a
 
-## Concern
+## Concerns
 - The first token id start at 0 (ERC721Enumerable start at 1)
+
+## Contracts
+Rinkeby: [0x428854D5a8201028a8a1537BE61ab310c804ec3c](https://rinkeby.etherscan.io/address/0x428854D5a8201028a8a1537BE61ab310c804ec3c)
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,9 +1,9 @@
-const ToonSurvival = artifacts.require("./ToonSurvival.sol");
+const ToonSurvival = artifacts.require('./ToonSurvival.sol');
 
 module.exports = (deployer, network, accounts) => {
-  // IPFS URI should have "/" at the end.
+  // IPFS URI should have '/' at the end.
   const baseUri = 'ipfs://xxxxx/';
-  const hiddenUri = "ipfs:/xxxxx/";
+  const hiddenUri = 'ipfs:/xxxxx/';
 
   deployer.deploy(ToonSurvival, baseUri, hiddenUri);
 };
